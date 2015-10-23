c45d93ccc81a3b77b014e33d55c4567cf49ce285
Christian Pape <cpape@posteo.de>
2015-10-23 08:18:40 +0200

    Dropdown naviation added.

--- resources/misc.js
+++ resources/misc.js
@@ -2,7 +2,7 @@
 $('a').click(function(){
     $('html, body').animate({
         scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
-    }, 1500);
+    }, 800);
     return false;
 });
 
