e1e13911c582d5b2bb7ea84883c132067f1fbb18
Christian Pape <cpape@posteo.de>
2015-10-23 07:20:58 +0200

    Some content added.

---
+++ resources/misc.js
@@ -0,0 +1,15 @@
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 1500);
+    return false;
+});
+
+
+// $('a').click(function(){
+//     $('html, body').animate({
+//         scrollTop: $( $.attr(this, 'href') ).offset().top
+//     }, 500);
+//     return false;
+// });
