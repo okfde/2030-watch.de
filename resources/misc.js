da8eb97b292b28ce1ec1e85bc34f72f1a223bd6c
Christian Pape <cpape@posteo.de>
2015-10-24 18:01:03 +0200

    First working draft for filter navigation.

--- resources/misc.js
+++ resources/misc.js
@@ -13,3 +13,13 @@ $('a').click(function(){
 //     }, 500);
 //     return false;
 // });
+
+// filter tabs
+$('#sdgFilter a').click(function (e) {
+    alert('SDG');
+
+});
+$('#respFilter a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+});
