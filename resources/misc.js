f835997a8e3e1eeb27f9ee506e90c42d41c844c8
Christian Pape <cpape@posteo.de>
2015-10-27 23:26:48 +0100

    Cookie added for ALPHA warning.

--- resources/misc.js
+++ resources/misc.js
@@ -54,3 +54,41 @@ var getIndicatorList = function () {
 }
 
 $('.alpha').tooltip();
+
+function setCookie(c_name,value,exdays)
+{
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value=escape(value) +
+        ((exdays==null) ? "" : ("; expires="+exdate.toUTCString()));
+    document.cookie=c_name + "=" + c_value;
+}
+
+function getCookie(c_name)
+{
+    var i,x,y,ARRcookies=document.cookie.split(";");
+    for (i=0;i<ARRcookies.length;i++)
+    {
+        x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+        y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+        x=x.replace(/^\s+|\s+$/g,"");
+        if (x==c_name)
+        {
+            return unescape(y);
+        }
+    }
+}
+
+var alertShown = getCookie("alpha");
+
+if (alertShown === "alertShown") {
+    setTimeout(function () {
+        $("#alphaAlert").fadeTo(5000,500).slideUp(500, function(){
+            $('#alphaAlert').alert('close');
+        });
+    });
+}
+
+$('#alphaAlertButton').on('click', function (e) {
+    setCookie('alpha', 'alertShown', 7);
+});
