4e57afff0dd8e5dadc6a1d6dab809101a3661e72
ffalt <ffalty@gmail.com>
2016-02-24 14:24:46 +0100

    housekeeping, removing duplicate/unneeded files

---
+++ static/js/index.js
@@ -0,0 +1,62 @@
+/**
+ * cbpAnimatedHeader.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2013, Codrops
+ * http://www.codrops.com
+ *
+ * + Fix for changing header on reload
+ */
+var cbpAnimatedHeader = (function() {
+
+    var docElem = document.documentElement,
+        header = $('.navbar-fixed-top'),
+        didScroll = false,
+        changeHeaderOn = 200;
+
+    function init() {
+        scrollPage();
+        window.addEventListener('scroll', function (event) {
+            if (!didScroll) {
+                didScroll = true;
+                setTimeout(scrollPage, 250);
+            }
+        }, false);
+    }
+
+    function scrollPage() {
+        var sy = scrollY();
+        if (sy >= changeHeaderOn) {
+            header.addClass('navbar-shrink');
+        } else {
+            header.removeClass('navbar-shrink');
+        }
+        didScroll = false;
+    }
+
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+
+    init();
+
+})();
+
+/*!
+ * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
