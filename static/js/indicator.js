d033ab186f461b2d6f1b0d5eb7c57852a0c8ea87
Knut Perseke <knut.perseke@gmail.com>
2016-03-14 14:39:35 +0100

    convert indicator page to angular app

--- static/js/indicator.js
+++ static/js/indicator.js
@@ -1,62 +1,73 @@
-/**
- * Created by knutator on 16.02.16.
- */
+var indicatorApp = angular.module('indicatorApp', ['angular.filter']);
 
-var currentIndicator = { indicator: null};
-var weekdays = ['Sonntage', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag']
+indicatorApp.controller('MainCtrl', function ($scope) {
+    $scope.indicators = indicatorProvider.getAllIndicators();
+    $scope.filteredIndicators = indicatorProvider.getAllIndicators();
+    $scope.datasource = false;
+    $scope.sdgs = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
 
-$(document).ready(function() {
+    var indicatorfilter = {};
 
-    rivets.bind($('#current-indicator'), currentIndicator);
-    indicatorProvider.getLastScoringByCountry();
+    $scope.setIndicator = function (id) {
+        console.log('show indicator ' + id);
+        $scope.activeIndicator = indicatorProvider.getIndicatorByIndex(id);
+        $scope.activeIndicator.lastScoring = indicatorProvider.getLastScoringForIndicator(id)
+    };
 
-    $('.indicator-filter').click(function(event) {
-       event.preventDefault();
-       $(this).closest('div').next().toggleClass('hidden');
-    })
-    $('.btn-danger').click(function(event) {
-        event.preventDefault();
-        toggleFilter('.btn-danger', 'active', this);
+    $scope.updateFilter = function(type, filter) {
+        console.log('update filter for ' + type + ", value: " + filter);
+        if (indicatorfilter[type] === filter)
+            delete indicatorfilter[type];
+        else
+            indicatorfilter[type] = filter;
+        refreshFilter();
+    };
+
+    $scope.isActiveDatasource = function(item) {
+        return indicatorfilter['datasource'] === item;
+    };
+
+    $scope.isActiveResponsibility = function(item) {
+        return indicatorfilter['responsibility'] === item;
+    };
+
+    $scope.isActiveSDG = function(item) {
+        return indicatorfilter['sdg'] === item;
+    };
+
+    function refreshFilter() {
+        var indicators = indicatorProvider.getAllIndicators();
+        console.log(indicatorfilter);
+        Object.keys(indicatorfilter).forEach(function(key,index) {
+            switch (key) {
+                case 'datasource':
+                    indicators = indicators.filter(function(item) {
+                        return item.indicator_source.value === indicatorfilter['datasource']
+                    });
+                    break;
+                case 'responsibility':
+                    indicators = indicators.filter(function(item) {
+                        return item.ministerial_responsibility.indexOf(indicatorfilter['responsibility']) > -1
+                    });
+                    break;
+                case 'sdg':
+                    indicators = indicators.filter(function(item) {
+                        return item.sdg.indexOf(indicatorfilter['sdg']) > -1
+                    });
+                    break;
+            }
+        });
+        $scope.filteredIndicators = indicators;
+        console.log(indicators);
+    }
+
+    var resps = [];
+    indicatorProvider.getAllIndicators().map(function(ind, index) {
+        if (ind.ministerial_responsibility)
+            ind.ministerial_responsibility.map(function(resp) {
+                console.log(index);
+                resps.push(resp)
+            });
     });
-    $('.btn-warning').click(function(event) {
-        event.preventDefault();
-        toggleFilter('.btn-warning', 'active', this);
-    });
-    $('.btn-primary').click(function(event) {
-        event.preventDefault();
-        toggleFilter('.btn-primary', 'active', this);
-    });
-    $('.indicator-element').click(function(event) {
-        event.preventDefault();
-        var me = this;
-        //alert('indicator '+ $(this).attr('value') + ' clicked');
-        var currentElement = indicators.find(function(item) {
-            return item['title'] === $(me).attr('value')});
-        console.log(currentElement);
-        currentIndicator.indicator = currentElement;
-    })
+    $scope.responsibilities = resps;
 });
-
-function toggleFilter(selector, classname, element) {
-    if ($(element).hasClass(classname)) {
-        $(element).removeClass(classname);
-    }
-    else
-    {
-        $(selector).removeClass(classname);
-        $(element).addClass(classname);
-    }
-    var activeFilters = {};
-    $('a.btn.active').map(function(index, item) {
-        activeFilters[$(item).attr('data-filter-type')] = $(item).attr('value');
-    });
-    filterIndicators(activeFilters);
-}
-
-function filterIndicators(filter) {
-    $('.hidden').removeClass('hidden');
-    $.each(filter, function(index, value) {
-        console.log('Index: ' + index + ' Value: ' + value);
-        $('li[' + index + '!=' + value + ']').addClass('hidden');
-    });
-}
