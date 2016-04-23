d96ef3ac5adb7a4f86b38bdd32331a409c494fe0
Letty <dittrich.katja@googlemail.com>
2016-04-23 16:46:24 +0200

    remove geraffel, add simple barchart without interaction and countryname

--- static/js/singleIndicators.js
+++ static/js/singleIndicators.js
@@ -9,190 +9,162 @@ var singleIndicatorSortOrder = 'down';
 var countryList = indicatorProvider.getSupportedCountries();
 
 // fill single indicators dropdown from database
-for(var i = 1; i <= indicators.length; i++) {
-    document.getElementById('indicatorSelector' + i).innerHTML=indicators[i-1]["title"];
+for (var i = 1; i <= indicators.length; i++) {
+	document.getElementById('indicatorSelector' + i).innerHTML = indicators[i - 1]["title"];
 }
 
 var fillIndicatorDetails = function (index) {
 
-    var indicator = indicatorProvider.getIndicatorByIndex(index);
-    var source = indicator.indicator_source ? indicator.indicator_source.value : "";
-    var dataSource = indicator.source_of_data ? indicator.source_of_data : "";
-    var year = indicator.most_recent_year ? indicator.most_recent_year : "";
-    var sdg = indicator.sdg ? indicator.sdg : "";
-    var datenpate = indicator.datenpate ? indicator.datenpate : "";
+	var indicator = indicatorProvider.getIndicatorByIndex(index);
+	var source = indicator.indicator_source ? indicator.indicator_source.value : "";
+	var dataSource = indicator.source_of_data ? indicator.source_of_data : "";
+	var year = indicator.most_recent_year ? indicator.most_recent_year : "";
+	var sdg = indicator.sdg ? indicator.sdg : "";
+	var datenpate = indicator.datenpate ? indicator.datenpate : "";
 
-    var responsibility = indicator.ministerial_responsibility;
-    var longResp = 'unbekannt';
+	var responsibility = indicator.ministerial_responsibility;
+	var longResp = 'unbekannt';
 
-    for(var i = 0; i < responsibilities.length; i++) {
-        if (responsibilities[i][responsibility] != undefined) {
-            longResp = responsibilities[i][responsibility];
-        }
-    };
+	for (var i = 0; i < responsibilities.length; i++) {
+		if (responsibilities[i][responsibility] != undefined) {
+			longResp = responsibilities[i][responsibility];
+		}
+	}
 
-    document.getElementById('indicatorDetailSource').innerHTML = source;
-    document.getElementById('indicatorDetailDataSource').innerHTML = dataSource;
-    document.getElementById('indicatorDetailResponsibility').innerHTML = responsibility;
-    document.getElementById('indicatorDetailYear').innerHTML = year;
-    document.getElementById('indicatorDetailSDGs').innerHTML = sdg;
-    if (datenpate.length > 0) {
-        document.getElementById('indicatorDetailDatenpate').innerHTML='Datenpate: ' + datenpate + '<br>';
-        $('#indicatorDetailDatenpate').css('display', 'inline');
-    } else {
-        $('#indicatorDetailDatenpate').css('display', 'none');
-    }
+	document.getElementById('indicatorDetailSource').innerHTML = source;
+	document.getElementById('indicatorDetailDataSource').innerHTML = dataSource;
+	document.getElementById('indicatorDetailResponsibility').innerHTML = responsibility;
+	document.getElementById('indicatorDetailYear').innerHTML = year;
+	document.getElementById('indicatorDetailSDGs').innerHTML = sdg;
+	if (datenpate.length > 0) {
+		document.getElementById('indicatorDetailDatenpate').innerHTML = 'Datenpate: ' + datenpate + '<br>';
+		$('#indicatorDetailDatenpate').css('display', 'inline');
+	} else {
+		$('#indicatorDetailDatenpate').css('display', 'none');
+	}
 };
 
 
 var barChart = function (dataIndex, order) {
-    $(function () {
 
-        if (dataIndex === null)
-            return false;
+	if (dataIndex === null) dataIndex = 0;
 
-        if (dataIndex === undefined)
-            dataIndex = singleIndicatorIndex;
+	d3.select('.indicatorBarChart').remove();
 
-        if (order === undefined)
-            order = singleIndicatorSortOrder;
+	singleIndicatorIndex = dataIndex;
+	singleIndicatorSortOrder = order;
 
-        singleIndicatorIndex = dataIndex;
-        singleIndicatorSortOrder = order;
+	var currentIndicator = indicatorProvider.getIndicatorByIndex(dataIndex);
+	var countries = indicatorProvider.getLastScoringByCountryForIndicator(dataIndex);
 
-        var currentIndicator = indicatorProvider.getIndicatorByIndex(dataIndex);
-        var countries = indicatorProvider.getLastScoringByCountryForIndicator(dataIndex);
+	var title = currentIndicator.title;
+	var unit = currentIndicator.baseunit;
+	var longDescription = currentIndicator.long_indicator_description.de;
 
-        var title = currentIndicator.title;
-        var unit = currentIndicator.baseunit;
-        var longDescription = currentIndicator.long_indicator_description.de;
+	if (longDescription === undefined)
+		longDescription = "";
 
-        if (longDescription === undefined)
-            longDescription = "";
+	//console.log(countries);
+	console.log(currentIndicator);
 
-        document.getElementById('longDescription').innerHTML = longDescription;
+	countries.sort(function (a, b) {
+		return b.value - a.value;
+	});
 
-        var collector = function (country) {
+	var begin = 0,
+		steps = 0;
+	countries.forEach(function (c, i) {
+		if (c.value === -1) {
+			if (begin === 0) begin = i;
+			steps++;
+		}
+	});
 
-            var value = ""
-            var score = 6;
+	countries.splice(begin, steps);
 
-            if (country) {
-                value = country.value;
-                score = country.score;
-            }
 
-            var color = colorScheme[score-1];
+	document.getElementById('longDescription').innerHTML = longDescription;
 
-            return {
-                name: translate(country.name),
-                data: [
-                    {
-                        y: value,
-                        score: score,
-                        color: color,
-                        name: translate(country.name)
-                    }
-                ],
-                pointPadding: 0,
-                groupPadding: 0.01
-            };
-        }
+	var margin = {
+		top: 10, bottom: 10, left: 50, right: 10
+	};
+	var width = document.getElementById('highchartsPane').clientWidth - margin.left - margin.right,
+		height = 400 - margin.bottom - margin.top;
 
-        var filterOut = function (data) {
-            return data.data[0].score != 6 && data.data[0].y != null && data.name;
-        };
+	var color = d3.scale.ordinal()
+		.domain([-1, 1, 2, 3, 4, 5])
+		.range(['#e0e0e0', '#d7191c', '#fdae61', '#ffe89d', '#abd9e9', '#2c7bb6']);
 
-        var sortUpPred = function (a, b) {
-            return a.data[0].y - b.data[0].y;
-        };
+	var x = d3.scale.ordinal()
+		.rangeRoundBands([0, width], 0.2)
+		.domain(countries.map(function (c) {
+			return c.name;
+		}));
 
-        var sortDownPred = function (a, b) {
-            return b.data[0].y - a.data[0].y;
-        };
+	var y = d3.scale.linear()
+		.range([height, 0])
+		.domain([0, d3.max(countries, function (d) {
+			return d.value;
+		})]);
 
-        var data;
+	var svg = d3.select('#highchartsPane').append('svg')
+		.attr('class', 'indicatorBarChart')
+		.attr('width', width + margin.left + margin.right)
+		.attr('height', height + margin.top + margin.bottom)
+		.append('g')
+		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-        if (order === 'up')
-            data = countries.map(collector).sort(sortUpPred);
+	svg.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(d3.svg.axis().scale(x).orient("bottom"));
 
-        if (order === 'down')
-            data = countries.map(collector).sort(sortDownPred);
+	svg.append("g")
+		.attr("class", "y axis")
+		.call(d3.svg.axis()
+			.scale(y)
+			.ticks(6)
+			.tickSize(-width)
+			.orient("left"))
+		.selectAll('text')
+		.attr('x', -8)
+		.append("text")
+		.attr("class", "label")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 5)
+		.attr("dy", ".71em")
+		.style("text-anchor", "end")
+		.text(title+' in '+unit);
 
-        if (order === 'standard')
-            data = countries.map(collector);
+	var rect = svg.selectAll(".bar")
+		.data(countries)
+		.enter().append("g")
+		.attr("class", "bar");
 
-        data = data.filter(filterOut);
+	rect.append('rect')
+		.attr("x", function (d) {
+			return x(d.name);
+		})
+		.attr("width", x.rangeBand())
+		.attr("y", function (d) {
+			return y(d.value);
+		})
+		.attr("height", function (d) {
+			return height - y(d.value);
+		})
+		.style('fill', function (d) {
+			return color(d.score);
+		});
+	//rect.append('text')
+	//	.attr("transform", "rotate(-90)")
+	//	.attr('y', height)
+	//	.text(function (d) {
+	//		return d.name;
+	//	});
 
-        $('#highchartsPane').highcharts({
-
-            chart: {type: 'column'},
-            title: {text: title},
-            xAxis: {categories: ['']},
-            yAxis: {title: {text: unit}},
-            plotOptions: {column: {colorByPoint: true}},
-            series: data,
-            plotOptions: {
-                column: {
-                    dataLabels: {
-                        enabled: true,
-                        align: 'center',
-                        inside: true,
-                        defer: false,
-                        allowOverlap: true,
-                        rotation: -90,
-                        y: -6,
-                        style: {"color": "black",
-                            "fontSize": "11px",
-                            "fontWeight": "normal",
-                            //"textShadow": "0 0 6px contrast, 0 0 3px contrast",
-                        },
-                        verticalAlign: 'middle',
-                        formatter: function() {
-                            return this.point.name;
-                        }
-                    }
-                },
-                pointPadding: 0
-            },
-            legend: {
-                enabled: false
-            },
-            tooltip: {
-                enabled: true,
-                useHTML: true,
-                borderRadius: 10,
-                borderWidth: 0,
-                shadow: false,
-                //style: 'height: 100px;',
-                shape: 'square'
-            }
-        });
-    });
-
-    fillIndicatorDetails(dataIndex);
-
-    var supplementaryViz;
-
-    try {
-        supplementaryViz = indicators[dataIndex]["supplementaryViz"];
-    }
-    catch (error) {
-        supplementaryViz = [];
-    }
-    if ( supplementaryViz === undefined)
-        supplementaryViz = [];
-
-    if ( supplementaryViz.length >= 1) {
-        $('#supplementaryRow').css('display', 'initial');
-        showSupplementary();
-    }
-    else {
-        $('#supplementaryRow').css('display', 'none');
-    };
-
+	fillIndicatorDetails(dataIndex);
 };
 
-$(document).ready(function() {
-    barChart(0);
+$(document).ready(function () {
+	barChart(0);
 });
