54a006590e1aa7cb3a8935f79388da31a0af6501
Letty <dittrich.katja@googlemail.com>
2016-06-03 11:56:35 +0200

    refactoring of site 'compare countries'

--- static/js/countries_vis.js
+++ static/js/countries_vis.js
@@ -1,3 +1,56 @@
+var countryApp = angular.module('CountryComparisonApp', [], function ($interpolateProvider) {
+    $interpolateProvider.startSymbol('[[');
+    $interpolateProvider.endSymbol(']]');
+});
+
+countryApp.controller('CompareCountryCtrl', function ($scope) {
+
+    $scope.country1 = { name: 'Deutschland'};
+    $scope.country2 = { name: 'Frankreich'};
+    $scope.country3 = { name: 'Schweden'};
+    $scope.countries = indicatorProvider.getSupportedCountries().map(function(d){
+        return {
+            name: translate(d)
+        };
+    }).sort(function (a, b) {
+        var nameA = a.name.toLowerCase(),
+           nameB = b.name.toLowerCase();
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+        return 0;
+    });
+
+    $scope.filter = ['Allgemeine Verteilung', 'Indikatoren'];
+    $scope.selctedfilter = $scope.filter[0];
+
+    $scope.changeFilter = function(){
+
+    };
+
+    //console.log($scope.countries);
+
+});
+
+countryApp.directive('compareViz', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            countries: '=',
+            binding: '=',
+            name: '='
+        },
+        templateUrl: 'compare_viz.html',
+        link: function (scope) {
+            scope.change = function (){
+
+            }
+        }
+    }
+});
 
 
 var country1 = 'France';
